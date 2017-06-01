0457a7d6aa31a97ef22dcda3ba1508b6b7f6a9eb
Emmet Murray <e.minor.murray@gmail.com>
2017-06-01 23:54:28 +1000

    stuff

--- bfs.c
+++ bfs.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "bbq.h"
 #include "bfs.h"
 
@@ -9,21 +10,30 @@
  * v is a pointer to the found node with key k
  */
 void search (Tree root, unsigned int N, Key key, T *val, RetVal *found) {
+	// Declare our circular buffer and two counters
 	Tree A[N+1]; int n; int m;
 	n = 0; m = 0;
+	// Set the initial value of the array
 	A[n] = root;
-	n = (n + 1) % N + 1;
-	found = Failure;
-	while (! (found || m == n)) {
+	// Increment n and set found
+	n = (n + 1) % (N + 1);
+	*found = Failure;
+	// While nothing is found or our queue is empty
+	while (! (*found || m == n)) {
+		// take our top element
 		Tree e = A[m];
-		m = (m + 1) % N + 1;
+		// remove it from the queue
+		m = (m + 1) % (N + 1);
+		// Check if we've found our value
 		if (cmpKey(key, e->id)) {
 			*val = e->val;
 			*found = Success;
+		// Add successors otherwise
 		} else {
 			List succ = e->list;
 			while (succ != NULL) {
-				A[n] = succ->n; n = (n + 1) % N + 1;
+				// Add the successor and increment list
+				A[n] = succ->n; n = (n + 1) % (N + 1);
 				succ = succ->next;
 			}
 		}
